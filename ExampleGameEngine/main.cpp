1fa0eedb9f8d1730106c253ec48c61872f10dc66
tylerhardy <tylerhardy@gmail.com>
2019-12-20 10:06:33 -0700

    Added frame rate limits

--- ExampleGameEngine/main.cpp
+++ ExampleGameEngine/main.cpp
@@ -4,14 +4,29 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int FrameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("HardyEngine", 800, 600, false);
 
 	while (game->running())
 	{
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();		// handle any user inputs
 		game->update();				// update all object eg. positions etc.
 		game->render();				// render changes to the display
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (FrameDelay > frameTime)
+		{
+			SDL_Delay(FrameDelay - frameTime);
+		}
 	}
 
 	game->clean();
